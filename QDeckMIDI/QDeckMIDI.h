df86a9d13f0ffc1482e64ee10a3aac82b10053e9
Matheus Meloni <13590967+matheusmeloni@users.noreply.github.com>
2022-02-21 12:06:38 -0300

    Update QDeckMIDI.h

--- QDeckMIDI/QDeckMIDI.h
+++ QDeckMIDI/QDeckMIDI.h
@@ -6,11 +6,11 @@
 
 namespace QDeckMIDI
 {
-    const byte CHANNEL = 0;
-    const byte MIDI_MIN  = 0;
-    const byte MIDI_MAX  = 127;
-    const int  POT_MIN = 10;
-    const int  POT_MAX = 1000;
+    const byte CHANNEL  = 0;
+    const byte MIDI_MIN = 0;
+    const byte MIDI_MAX = 127;
+    const int  POT_MIN  = 10;
+    const int  POT_MAX  = 1000;
 
     void noteOn(byte pitch, byte velocity);
     void noteOff(byte pitch, byte velocity);
