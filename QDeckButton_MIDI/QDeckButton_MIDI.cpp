056ce44d67c6c3346733223cfdfb0311afde5fea
Matheus Meloni <13590967+qqquiu@users.noreply.github.com>
2022-03-06 15:58:44 -0300

    Delete QDeckButton_MIDI directory

--- QDeckButton_MIDI/QDeckButton_MIDI.cpp
+++
@@ -1,9 +0,0 @@
-#include "QDeckButton_MIDI.h"
-
-void QDeckButton_MIDI::Interact()
-{
-    QDeckMIDI::noteOn(_note, QDeckMIDI::MIDI_MAX);
-    MidiUSB.flush();
-    QDeckMIDI::noteOff(_note, QDeckMIDI::MIDI_MAX);
-    MidiUSB.flush();
-}
