4ab949130a0fdb7bf9329569658fa2f942473d3b
matheusmeloni <matheusmelonip@gmail.com>
2022-05-23 08:16:50 -0300

    Updated examples and moved into a separate folder

---
+++ examples/pot_example.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#include <QDeck_Midi.h>
+#include <QDeck_Pot.h>
+
+const uint8_t pot_pin = 0;
+const uint8_t pot_sig = 11; // node 11 in midi
+
+// We're using QDeck::Pot here but there are typedefs for QDeck::Fader and QDeck::Potentiometer
+QDeck::Pot pots[] =
+{
+    new QDeck::Pot {pot_pin, pot_sig}
+}
+
+// This is defined in QDeck_Pot.h so we don't have to keep track of pots[] length
+const size_t total_pots = GET_TOTAL_POTS(pots);
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    for (int i = 0; i < total_pots; i++)
+    {
+        pots[i]->Read();
+    }
+}
---
+++ examples/switch_example.ino
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <Keyboard.h>
+#include <MIDIUSB.h>
+
+#include <QDeck_Switch.h>
+#include <QDeck_KbSwitch.h>
+#include <QDeck_Midi.h>
+#include <QDeck_MidiSwitch.h>
+#include <QDeck_SwitchManager.h>
+
+// We need to have a common pin to wire all switches together. It has to be interrupt compatible
+const uint8_t interrupt_pin = 0;
+
+// Define pins and key signals for keyboard switches
+const uint8_t key_pin = 7;
+const uint8_t key_sig = KEY_F13; // these constants are defined in Keyboard.h
+
+// Define pins and notes for midi switches
+const uint8_t midi_pin = 8;
+const uiint8_t midi_sig = 10; // note 10 in midi
+
+QDeck::Switch* switches[] = 
+{
+    new QDeck::KeySwitch  {key_pin, key_sig},
+    new QDeck::MidiSwitch {midi_pin, midi_sig}
+    // Add more switches here as desired. Because it's using the base Switch class,
+    // you can mix MIDI and Keyboard switches as the manager class will call a virtual function
+    // which will result in the proper subroutines being called
+
+    // Depending on the amount of switches and the rest of your code, be wary of dynamic memory usage
+}
+
+// This is defined in QDeck_Switch.h so we don't have to keep track of switches[] length
+const size_t total_switches = GET_TOTAL_SWITCHES(switches);
+
+// Button presses will register as interrupts and will be handled by the manager class.
+// No additional code is required.
+QDeck::SwitchManager manager = {switches, total_switches, interrupt_pin};
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    
+}
